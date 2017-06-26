3e0e807df6b6a88694e091b6905e96d7f1261ee2
Ellyn737 <e.reese@mail.de>
2017-06-26 19:27:18 +0200

    finished SignUp plus Profile

--- app/src/main/java/com/example/ellyn/challenger/SignUp.java
+++ app/src/main/java/com/example/ellyn/challenger/SignUp.java
@@ -1,7 +1,9 @@
 package com.example.ellyn.challenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void backToMain(View view){
+        //open the MainActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openProfile(View view){
+        //open the ProfileActivity
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
