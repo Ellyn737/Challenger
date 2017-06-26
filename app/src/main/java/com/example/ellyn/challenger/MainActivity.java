469e07f7a72c64af45b8abd5eab83b4de60ba83c
Ellyn737 <e.reese@mail.de>
2017-06-26 18:55:52 +0200

    with SignIn and SIgnUp

--- app/src/main/java/com/example/ellyn/challenger/MainActivity.java
+++ app/src/main/java/com/example/ellyn/challenger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ellyn.challenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view){
+        //open the signUpActivity
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+    public void signIn(View view){
+        //open the SignInActivity
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
     }
 }
